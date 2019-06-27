0252e3759c5a319b1c6944913ee489842807b5d9
Leandro Moreira <leandro.ribeiro.moreira@gmail.com>
2019-06-27 11:11:07 -0300

    initial projects contest

---
+++ start_http_server.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+cd /tmp/
+
+finish()
+{
+  cd -
+}
+
+trap finish EXIT
+
+docker run --rm -v $(pwd):/opt/www -p 8080:8080 webratio/nodejs-http-server:0.9.0 http-server /opt/www -a :: -p 8080 --cors -c-1
+
